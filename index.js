f5ef19d587bff76d02751f7e85e65a9eed0aeb96
VolodymyrTrush <vmtrush@gmail.com>
2022-09-29 19:33:18 +0300

    HW1

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const contacts = require("./contacts");
-
+// const { program } = require("commander");
 const { Command } = require("commander");
-
 const program = new Command();
-
-program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
-  
-program.parse(process.argv);
-  
-const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -44,5 +32,16 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       console.warn("\x1B[31m Unknown action type!");
   }
 };
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 invokeAction(argv);
